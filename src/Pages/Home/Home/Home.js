7fb4c8a0b06f3b6c16a6827846f4cc340cdc6293
mss38 <mss38k@gmail.com>
2022-05-08 16:30:33 +0600

    added loading home page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Services from "../../Inventory/Services/Services/Services";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import Loading from "../../Login/Loading/Loading";
 
 const Home = () => {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
       <Banner></Banner>
